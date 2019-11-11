7d712ccbc3b9a473c95b5c081abdcb52de08b967
Luís Barbosa <luis.barbosa@ebankit.com>
2019-11-11 17:46:20 +0000

    Test

--- Sources/MyLibraryTest/MyLibraryTest.swift
+++ Sources/MyLibraryTest/MyLibraryTest.swift
@@ -1,3 +1,15 @@
 struct MyLibraryTest {
     var text = "Hello, World!"
 }
+
+private class PrivateClass {
+	func pri() -> String {
+		return "pri"
+	}
+}
+
+public class PublicClass {
+	func pub() -> String {
+		return "pub"
+	}
+}
